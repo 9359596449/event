8b1628d47e6986dab8fe9e9997be6d49744424fc
9359596449 <100253652+9359596449@users.noreply.github.com>
2025-06-22 23:25:21 +0530

    Update config.php

--- config.php
+++ config.php
@@ -6,8 +6,8 @@
     ],
     'qr_target' => 'http://localhost/Event/view.php',
     'upload_dirs' => [
-        'bride' => __DIR__ . '/uploads/bride/',
-        'groom' => __DIR__ . '/uploads/groom/'
+        'bride' => __DIR__ . '/upload/bride/',
+        'groom' => __DIR__ . '/upload/groom/'
     ]
     ];
 ?>
